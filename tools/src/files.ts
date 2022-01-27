b26079b3de5fe54bd5373a2c4b57cdf23eb891f0
wslyvh <wslyvh@users.noreply.github.com>
2022-01-27 11:38:49 +0100

    files

--- tools/src/files.ts
+++ tools/src/files.ts
@@ -2,14 +2,8 @@ import { join } from 'path'
 import fs from 'fs'
 
 const main = async () => {
-  const current = join(__dirname)
-  console.log('Files in', current)
-  fs.readdirSync(current, { withFileTypes: true }).forEach(i => {
-    console.log('  -', i.name)
-  })
-
   const data = join(__dirname, '../../data')
-  console.log('Files in', data)
+  console.log('Files in data directory', data)
   fs.readdirSync(data, { withFileTypes: true }).forEach(i => {
     console.log('  -', i.name)
   })
